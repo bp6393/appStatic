51dc7012454ae861c1f42a72630e05c37c061a09
bp6393 <107973318+bp6393@users.noreply.github.com>
2022-06-22 08:42:47 +0530

    Add files via upload

---
+++ client/src/components/Posts/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@mui/styles';
+
+// export default makeStyles((theme) => ({
+export default makeStyles(() => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smMargin: {
+    margin: 8,
+    // margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: 'center',
+  },
+}));
